f9655bcf8ed6a7f91a2991c7b7973ca43cb0894b
an nguyen <hello.an.ng@gmail.com>
2017-02-27 11:06:41 -0800

    moves specs and schedule to specs dir

---
+++ src/Map.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+const LA_ARTS_DIST_POS = {
+  lat: 34.0463342,
+  lng: -118.2352291
+};
+
+
+class Map extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.map = new window.google.maps.Map(this.refs.map, {
+      center: LA_ARTS_DIST_POS,
+      zoom: 17
+    });
+  }
+
+  render() {
+    const mapStyle = {
+      width: '100%',
+      height: 800,
+      border: '1px solid black'
+    };
+
+    return (
+      <div>
+        <div ref="map" style={mapStyle}>Map goes here</div>
+      </div>
+    );
+  }
+}
+
+export default Map;
