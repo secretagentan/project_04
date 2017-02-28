b226a702bd430911cd0433381b16efd0b8ff6c50
an nguyen <hello.an.ng@gmail.com>
2017-02-28 12:45:13 -0800

    moves nav to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <nav className="lighten-3">
+            <div className="nav-wrapper">
+              <a href="/" className="page-title brand-logo"> m u r a l . l a </a>
+              <ul className="right hide-on-med-and-down">
+                <li><a href="#">login</a></li>
+                <li><a href="#">gallery</a></li>
+                <li><a href="#">about</a></li>
+              </ul>
+            </div>
+          </nav>
+        </header>
         <UploadForm />
         <Map />
       </div>
