8cffb1a6e84c7fb0df5aa6dccd2f4656ba0eb11f
an nguyen <hello.an.ng@gmail.com>
2017-02-28 19:12:01 -0800

    cleans up files

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends React.Component {
     return(
       <nav className="lighten-3">
         <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">m u r a l . l a</Link>
+          <span className="brand-logo center"><Link to="/">m u r a l . l a</Link></span>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to="/map">map</Link></li>
             <li><Link to="/upload">upload</Link></li>
