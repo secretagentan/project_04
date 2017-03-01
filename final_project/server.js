0055dba444e3b38a80e9e1b152a330dff455cab8
an nguyen <hello.an.ng@gmail.com>
2017-02-28 21:39:01 -0800

    saves empty server.js file

---
+++ final_project/server.js
@@ -0,0 +1,6 @@
+// var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/sandbox';
+
+// var port = process.env.PORT || 3000;
+// app.listen(port, function() {
+//   console.log('Listening on port: ' + port);
+// });
