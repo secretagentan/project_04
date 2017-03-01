7060f42d76e08827d7ce513e452f41d9d1b89a71
an nguyen <hello.an.ng@gmail.com>
2017-03-01 15:02:15 -0800

    centers background and changes nav opacity

--- final_project/src/ImgGallery.js
+++ final_project/src/ImgGallery.js
@@ -9,10 +9,16 @@ class ImgGallery extends React.Component {
     return (
       <div className="gallery">
         <img className="gallery-img" src='http://i.imgur.com/p7ocU8O.jpg' alt="img2" />
-        <p>posted by: antheman</p>
+        <p>posted by: secretagentan</p>
         <p>location: los angeles, ca</p>
         <p>lat: 34.046202</p>
         <p>long: -118.2379302</p>
+        <br/><br/>
+        <img className="gallery-img" src='http://i.imgur.com/K2Wboi8.jpg' alt="img2" />
+        <p>posted by: antheman</p>
+        <p>location: los angeles, ca</p>
+        <p>lat: 34.045626</p>
+        <p>long: -118.2371662</p>
       </div>
     );
   }
--- final_project/src/NavBar.js
+++ final_project/src/NavBar.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router';
 class NavBar extends React.Component {
   render() {
     return(
-      <nav className="lighten-3">
+      <nav className="my-nav lighten-3">
         <div className="nav-wrapper">
           <span className="brand-logo center"><Link to="/">m u r a l . l a</Link></span>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to="/gallery">gallery</Link></li>
             <li><Link to="/map">map</Link></li>
-            <li><Link to="/upload">upload</Link></li>
+            <li><Link to="/upload">contribute</Link></li>
           </ul>
         </div>
       </nav>
