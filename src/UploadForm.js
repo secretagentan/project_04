ec3b339351a81a907eadec319cc038b76544a402
an nguyen <hello.an.ng@gmail.com>
2017-03-01 20:01:35 -0800

    connects form input fields to state

--- src/UploadForm.js
+++ src/UploadForm.js
@@ -3,27 +3,49 @@ import React from 'react';
 class UploadForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      imgUrl: 'a',
+      username: 'b',
+      note: 'c',
+      lat: 'd',
+      lng: 'e'
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="upload-form">
-      <form action="#">
-        <div className="file-field input-field">
-          <div className="file-path-wrapper">
-            <input type="text" placeholder="username" />
-            <input type="text" placeholder="enter image url" />
-            <input type="text" placeholder="city, state" />
-            <input type="text" placeholder="latitude" />
-            <input type="text" placeholder="longitude" />
-            <button className="btn right waves-effect waves-light" type="submit" name="action">Submit
-              <i className="material-icons right">send</i>
-            </button>
+        <form action="/places" method="post" onSubmit={this.handleSubmit}>
+          <div className="file-field input-field">
+            <div className="file-path-wrapper">
+              <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} />
+              <input type="text" name="imgUrl" placeholder="image url" value={this.state.imgUrl} onChange={this.handleChange} />
+              <input type="text" name="note" placeholder="city, state" value={this.state.note} onChange={this.handleChange} />
+              <input type="text" name="lat" placeholder="latitude" value={this.state.lat} onChange={this.handleChange} />
+              <input type="text" name="lng" placeholder="longitude" value={this.state.lng} onChange={this.handleChange} />
+              <button className="btn right waves-effect waves-light" type="submit" name="action">Submit
+                <i className="material-icons right">send</i>
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-      <br />
-      <br />
+        </form>
+        <br />
+        <br />
       </div>
     );
   }
