d556543fee404572c307019eb929285460a989bb
an nguyen <hello.an.ng@gmail.com>
2017-03-02 21:05:56 -0800

    attempts react hamburger styling but failed

--- final_project/src/NavBar.js
+++ final_project/src/NavBar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class NavBar extends React.Component {
+
   render() {
     return(
       <nav className="my-nav lighten-3">
         <div className="nav-wrapper">
           <span className="brand-logo center"><Link to="/">m u r a l . l a</Link></span>
-          <ul className="right hide-on-med-and-down">
+          <ul className="right">
             <li><Link to="/upload">contribute</Link></li>
             <li><Link to="/map">map</Link></li>
             <li><Link to="/gallery">gallery</Link></li>
