8b9ccd532f3b9e4b71f46bef62ae3980baa75448
an nguyen <hello.an.ng@gmail.com>
2017-03-02 18:00:55 -0800

    yay kora fixed it

--- src/ImgGallery.js
+++ src/ImgGallery.js
@@ -29,38 +29,34 @@ class ImgGallery extends React.Component {
     // this.retrieveData();
     const data = this.state.data;
     // console.log(data);
-    // const results = data.map( (key, val) => <div key={key}>{val}</div>)
+    const results = data.map( (dat, index) => <Kitten key={index} data={dat} />)
 
     return (
       <div className="gallery">
-
-        <img className="gallery-img" src={this.state.data} alt="img-test" />
-        <p>posted by: {this.props.created_by}</p>
-        <p>location: {this.props.note}</p>
-        <p>lat: {this.props.lat}</p>
-        <p>long: {this.props.lng}</p>
-
-        <br/><br/>
-        <img className="gallery-img" src='http://i.imgur.com/CrEk1Q3.jpg' alt="img1" />
-        <p>posted by: secretagentan</p>
-        <p>location: los angeles, ca</p>
-        <p>lat: 34.046202</p>
-        <p>long: -118.2379302</p>
-        <br/><br/>
-        <img className="gallery-img" src='http://i.imgur.com/p7ocU8O.jpg' alt="img2" />
-        <p>posted by: antheman</p>
-        <p>location: los angeles, ca</p>
-        <p>lat: 34.045626</p>
-        <p>long: -118.2371662</p>
-        <br/><br/>
-        <img className="gallery-img" src='http://i.imgur.com/K2Wboi8.jpg' alt="img3" />
-        <p>posted by: agentxhippo</p>
-        <p>location: los angeles, ca</p>
-        <p>lat: 34.045626</p>
-        <p>long: -118.2371662</p>
+        {results}
       </div>
     );
   }
 };
+
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props
+    }
+  }
+  render() {
+    return (
+      <div>
+        <img className="gallery-img" src={this.props.data.img_url} alt="img-test" />
+        <p>posted by: {this.props.data.created_by}</p>
+        <p>location: {this.props.data.note}</p>
+        <p>lat: {this.props.data.lat}</p>
+        <p>long: {this.props.data.lng}</p>
+      </div>
+      )
+  }
+}
 
 export default ImgGallery;
