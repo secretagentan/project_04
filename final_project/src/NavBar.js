3deb7f5fb571c44b5df01d9b924ba53a57d97391
an nguyen <hello.an.ng@gmail.com>
2017-03-02 20:19:33 -0800

    removes hamburger attempt

--- final_project/src/NavBar.js
+++ final_project/src/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends React.Component {
       <nav className="my-nav lighten-3">
         <div className="nav-wrapper">
           <span className="brand-logo center"><Link to="/">m u r a l . l a</Link></span>
-          <ul className="right">
+          <ul className="right hide-on-med-and-down">
             <li><Link to="/upload">contribute</Link></li>
             <li><Link to="/map">map</Link></li>
             <li><Link to="/gallery">gallery</Link></li>
