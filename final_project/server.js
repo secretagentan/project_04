d83cceeb032e7a6182f47fd141f345f970424c20
an nguyen <hello.an.ng@gmail.com>
2017-03-03 11:49:53 -0800

    i hope this works

--- final_project/server.js
+++
@@ -1,6 +0,0 @@
-// var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/sandbox';
-
-// var port = process.env.PORT || 3000;
-// app.listen(port, function() {
-//   console.log('Listening on port: ' + port);
-// });
